a078ad932f3c29d8eeb2bffe586381c8fd120267
Smit Mistry <smitmistry19@gmail.com>
2026-05-26 12:13:05 +0530

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+  adapter: vercel(),
 });
